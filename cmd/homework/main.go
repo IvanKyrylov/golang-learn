2c9cd09c3f96bbc9eb27c6292601e0c4b42092c6
Ivan Kyrylov <vannsha.may@gmail.com>
2020-07-03 15:35:35 +0300

    Refactoring

--- cmd/homework/main.go
+++ cmd/homework/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "github.com/IvanKyrylov/goland_learn/cmd/homework/correcttime"
-
 func main() {
-	correcttime.CorrectTime()
+
 }
--- cmd/homework/taskone/correctTime.go
+++ cmd/homework/taskone/correctTime.go
@@ -1,4 +1,4 @@
-package correcttime
+package taskone
 
 import (
 	"fmt"
