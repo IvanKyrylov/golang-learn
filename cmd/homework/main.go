b40e7ac7114e2b36b8d65aeb36bb4b63bc25e056
Ivan Kyrylov <vannsha.may@gmail.com>
2020-07-04 00:31:40 +0300

    Revert changing

--- cmd/homework/main.go
+++ cmd/homework/main.go
@@ -1,4 +1,7 @@
 package main
 
+import "github.com/IvanKyrylov/goland-learn/internal/app/homework/taskone"
+
 func main() {
+	taskone.CorrectTime()
 }
--- internal/app/homework/taskone/taskone_test.go
+++ internal/app/homework/taskone/taskone_test.go
@@ -1,4 +1,4 @@
-package one
+package taskone
 
 import (
 	"testing"
