64bc50d5f4e0322509af6bc9b3c04cd460bda304
IvanKyrylov <vannsha.may@gmail.com>
2020-10-18 16:37:59 +0300

    Refactoring

--- internal/app/homework/taskone/taskone.go
+++ internal/app/homework/taskone/taskone.go
@@ -10,17 +10,14 @@ import (
 
 const server = "3.pool.ntp.org"
 
-func HelloNow() time.Time {
+func HelloNow() {
 	localTime := time.Now()
 	timeNtp, err := ntp.Time(server)
 
 	if err != nil {
 		log.Fatalf("Error: %s", err)
-		return timeNtp
 	}
 
 	fmt.Printf("Local time: %v\n", localTime.Round(time.Nanosecond).String())
 	fmt.Printf("Exact time: %v\n", timeNtp.Round(time.Nanosecond).String())
-
-	return timeNtp
 }
